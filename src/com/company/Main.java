710b6b76401fd95d7a632eaa4790cb9668dec655
Adahan <adahankasymaliev24@gmail.com>
2021-11-09 17:55:35 +0600

    исправил ошибку

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,20 +9,24 @@ public class Main {
         adahan.setShield("Барьер");
         System.out.println("Adahan " + adahan.getHealth() + " XP " + adahan.getDamage() + " DD " + adahan.getShield() + " PP");
 
-creatHeroes();
+        Hero[] marvel = creatHeroes();
+
+        for (int i = 0; i < marvel.length; i++) {
+
+            System.out.println(marvel[i].getName() + " " + marvel[i].getDamage() + " " + marvel[i].getHealth() + " " + marvel[i].getPower());
+
+
+        }
     }
 
+
     public static Hero[] creatHeroes() {
+
         Hero hulk = new Hero(300, 25, "forse", "hulk");
         Hero flash = new Hero(250, 30, "speed", "flash");
         Hero thor = new Hero(350, 35, "electricity", "thor");
         Hero[] marvel = {hulk, flash, thor};
-        for (int i = 0; i < marvel.length; i++) {
-            System.out.println(marvel[i].getName() + " " + marvel[i].getDamage() + " " + marvel[i].getHealth() + " " + marvel[i].getPower());
 
-
-
-        }
         return marvel;
     }
 }

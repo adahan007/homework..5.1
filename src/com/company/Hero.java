97347b625f5edd822eda09d32f5f7d6aab39005b
Adahan <adahankasymaliev24@gmail.com>
2021-11-09 16:27:25 +0600

    fhcfncf

--- src/com/company/Hero.java
+++ src/com/company/Hero.java
@@ -1,12 +1,20 @@
+package com.company;
+
 public class Hero {
     private int health;
     private int damage;
     private String power;
+    private String name;
 
-    public Hero(int health, int damage, String power) {
+    public String getName() {
+        return name;
+    }
+
+    public Hero(int health, int damage, String power, String name) {
         this.health = health;
         this.damage = damage;
         this.power = power;
+        this.name = name;
     }
 
     public Hero(int health, int damage) {
@@ -25,5 +33,6 @@ public class Hero {
     public String getPower() {
         return power;
     }
+
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,30 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	Boss Adahan = new Boss();
-	Adahan.setHealth(1000);
-	Adahan.setDamage(90);
-	Adahan.setShield("Барьер");
-        System.out.println("Adahan " + Adahan.getHealth() + " XP " + Adahan.getDamage() + " DD " + Adahan.getShield()+" PP");
+        Boss adahan = new Boss();
+        adahan.setHealth(1000);
+        adahan.setDamage(90);
+        adahan.setShield("Барьер");
+        System.out.println("Adahan " + adahan.getHealth() + " XP " + adahan.getDamage() + " DD " + adahan.getShield() + " PP");
+
+creatHeroes();
+    }
+
+    public static Hero[] creatHeroes() {
+        Hero hulk = new Hero(300, 25, "forse", "hulk");
+        Hero flash = new Hero(250, 30, "speed", "flash");
+        Hero thor = new Hero(350, 35, "electricity", "thor");
+        Hero[] marvel = {hulk, flash, thor};
+        for (int i = 0; i < marvel.length; i++) {
+            System.out.println(marvel[i].getName() + " " + marvel[i].getDamage() + " " + marvel[i].getHealth() + " " + marvel[i].getPower());
 
 
+        }
+        return marvel;
     }
 }
+
+
+
+
+
